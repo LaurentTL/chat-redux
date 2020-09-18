378c5b1dd4d9dbe7c3f7f2e1d5f08e253e803750
Laurent Treuil <laurent.treuil@gmail.com>
2020-09-18 10:19:29 +0200

    ok

--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddlewares } from 'redux';
-import { logger } from 'redux-logger';
-import { reduxPromise } from 'redux-promise';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
-import messagesReducer from '../reducers/messages_reducer.js';
-import selectedChannelReducer from '../reducers/selected_channel_reducer.js';
+import messagesReducer from './reducers/messages_reducer';
+import selectedChannelReducer from './reducers/selected_channel_reducer';
 
 // State and reducers
 const identityReducer = (state = null) => state;
 
 const initialState = {
   messages: [],
-  channels: ['general', 'Paris', 'react', 'RubyOnRails'],
+  channels: ['general', 'Paris', 'react'],
   selectedChannel: 'general',
   currentUser: prompt("What is your username?") || `anonymous${Math.floor(10 + (Math.random() * 90))}`
 };
@@ -30,12 +30,13 @@ const reducers = combineReducers({
   selectedChannel: selectedChannelReducer
 });
 
-const middlewares = applyMiddlewares(logger, reduxPromise);
+const middlewares = applyMiddleware(logger, reduxPromise);
+const store = createStore(reducers, initialState, middlewares);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers, initialState, middlewares)}>
+  <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById('app')
 );
